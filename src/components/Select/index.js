2b94159523711f63b0c1962ebeb1d05c688cdbe9
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-02-20 15:58:25 +0100

    added newValue to onChange to filter categories

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -16,7 +16,7 @@ const Select = ({
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange();
+    onChange(newValue); // added newValue to onChange
     setValue(newValue);
     setCollapsed(newValue);
   };
